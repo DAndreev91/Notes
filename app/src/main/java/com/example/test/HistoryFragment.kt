692d95a7925cda2180ba59d9f65dc99a45306582
DAndreev91 <andreevds91@gmail.com>
2024-02-09 23:56:32 +0300

    Try fixing click on note

--- app/src/main/java/com/example/test/HistoryFragment.kt
+++ app/src/main/java/com/example/test/HistoryFragment.kt
@@ -105,7 +105,7 @@ class HistoryFragment : Fragment() {
     }
 
     private fun openDialog (pos: Int) {
-        val dialog = NoteDialog(noteViewModel)
+        val dialog = NoteDialog()
         dialog.setDialogNote(pos)
         dialog.setTextNonEditable()
         dialog.show(parentFragmentManager, "1")
